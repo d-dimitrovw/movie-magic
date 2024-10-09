2e6c2510bcb8b888e720782f7b624904e6cfadb3
d-dimitrovw <daviddimitrov2004@gmail.com>
2024-10-09 15:30:24 +0300

    Setup express and initiate server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>Just Works</h1>')
+})
+
+app.listen(5000, () => console.log('Server is running on port 5000...'));
