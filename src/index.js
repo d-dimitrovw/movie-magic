72e4be9f37d08a80cf20de10c95346b9b4ccf029
d-dimitrovw <daviddimitrov2004@gmail.com>
2024-10-09 15:37:47 +0300

    Setup handlebars

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
 
 const app = express();
 
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}))
+app.set('view engine', 'hbs');
+app.set('views', './src/views')
+
 app.get('/', (req, res) => {
-    res.send('<h1>Just Works</h1>')
+    res.render('index', {layout: false})
 })
 
 app.listen(5000, () => console.log('Server is running on port 5000...'));
