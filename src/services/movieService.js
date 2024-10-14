53357755a7d741eba6cc2b7c5d26872999da05ba
d-dimitrovw <daviddimitrov2004@gmail.com>
2024-10-14 23:26:05 +0300

    Add details page

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -9,7 +9,15 @@ const create = (movie) => {
     return movieData.create(movie);
 }
 
+const getOne = async (movieId) => {
+    const movies = await movieData.getAll();
+
+    const resultMovie = movies.find(movie => movie.id === movieId);
+    return resultMovie;
+}
+
 export default {
     getAll,
-    create
+    create,
+    getOne
 };
