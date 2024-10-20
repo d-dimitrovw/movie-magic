2a851eeaca8b7e7ab52aebfcd60e881a63d07a5d
d-dimitrovw <daviddimitrov2004@gmail.com>
2024-10-20 21:54:03 +0300

    show cast in details page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -29,7 +29,7 @@ router.get('/search', async (req, res) => {
 
 router.get('/:movieId/details', async (req, res) => {
     const movieId = req.params.movieId;
-    const movie = await movieService.getOne(movieId).lean();
+    const movie = await movieService.getOne(movieId).lean();    
 
     movie.ratingView = getRatingViewData(movie.rating);
 
@@ -41,8 +41,18 @@ router.get('/:movieId/attach', async (req, res) => {
 
     const casts = await castService.getAll().lean();
 
+    
     res.render('movies/attach', { movie, casts });
 
+})
+
+router.post('/:movieId/attach', async (req, res) => {
+    const movieId = req.params.movieId;
+    const castId = req.body.cast;
+
+    await movieService.attach(movieId, castId);
+
+    res.redirect(`/movies/${movieId}/details`)
 })
 
 function getRatingViewData(rating) {
