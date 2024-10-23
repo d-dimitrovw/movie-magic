27e9410f209c3a3fb5379f6c2631e7482ca8d1f9
d-dimitrovw <daviddimitrov2004@gmail.com>
2024-10-23 15:01:07 +0300

    generate jwt token on successful login

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,4 +1,8 @@
-import User from "../models/User.js"
+import User from "../models/User.js";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const SECRET = '4trgfudot83hfnv9eru';
 
 const register = (email, password) => {
     return User.create({ email, password });
@@ -6,11 +10,29 @@ const register = (email, password) => {
 
 const login = async (email, password) => {
     const user = await User.findOne({ email });
-    
+
     // check if user exists
     if (!user) {
         throw new Error('User does not exist!');
     }
+
+    // validate password
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) {
+        throw new Error('Password does not match!');
+    }
+
+    // generate jwt token
+    const payload = { 
+        _id: user._id,
+        email,
+    }
+
+    const token = jwt.sign(payload, SECRET, { expiresIn: '2h' });
+
+    return token;
+
+    // return jwt token
 
 }
 
