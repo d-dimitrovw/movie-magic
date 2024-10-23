aa9e2e248c547ba333f87bcac02babf9ff16d5f0
d-dimitrovw <daviddimitrov2004@gmail.com>
2024-10-23 14:20:15 +0300

    validate login password

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -15,4 +15,16 @@ router.post('/register', async (req, res) => {
     res.redirect('/auth/login');
 });
 
+router.get('/login', (req, res) => {
+    res.render('auth/login');
+})
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    const token = await authService.login(email, password);
+
+    res.redirect('/')
+})
+
 export default router;
--- src/services/authService.js
+++ src/services/authService.js
@@ -4,6 +4,17 @@ const register = (email, password) => {
     return User.create({ email, password });
 }
 
+const login = async (email, password) => {
+    const user = await User.findOne({ email });
+    
+    // check if user exists
+    if (!user) {
+        throw new Error('User does not exist!');
+    }
+
+}
+
 export default {
     register,
+    login,
 }
